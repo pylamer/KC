7d639e6242014d0a2d4adea3ab833f01484804ca
Valeriy <knyazvs85@gmail.com>
2023-05-06 17:55:09 +0300

    test commit

--- tests.py
+++ tests.py
@@ -1,5 +1,17 @@
-from utils import get_secret_number
+import redis
 
-URL_SEC_NUMBER = 'https://lab.karpov.courses/hardml-api/module-5/get_secret_number'
+app_name = "web_app"
+replic_name = "replic1"
+host_name = "95.216.191.176"
+port_name = 5000
 
-print(get_secret_number(URL_SEC_NUMBER))
+redis_client = redis.Redis(host="localhost", port=6379, db=0)
+redis_client.rpush(app_name, replic_name)
+
+redis_client.hset(replic_name, "host_name", host_name)
+redis_client.hset(replic_name, "port_name", port_name)
+a = redis_client.rpop(app_name)
+print(a)
+redis_client
+
+redis_client.close()
