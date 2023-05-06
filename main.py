d16a2064af3ea7f8b44aea9df8035291e4407a04
Valeriy <knyazvs85@gmail.com>
2023-05-06 19:50:37 +0300

    test commit

--- main.py
+++ main.py
@@ -23,7 +23,8 @@ if __name__ == '__main__':
     host_name = "95.216.191.176"
     port_name = 5000
 
-    redis_client = redis.Redis(host="localhost", port=6379, db=0, password='lolkek123')
+    # redis_client = redis.Redis(host="localhost", port=6379, db=0, password='lolkek123')
+    redis_client = redis.Redis(host="172.17.0.2", port=6379, db=0, password='lolkek123')
     redis_client.rpush(app_name, replic_name)
 
     redis_client.hset(replic_name, "host", host_name)
