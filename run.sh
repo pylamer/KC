c7052c4b6471b9b0289626c4760661b04bd24c7e
Valeriy <knyazvs85@gmail.com>
2023-05-07 13:16:08 +0300

    test commit

--- run.sh
+++ run.sh
@@ -2,3 +2,7 @@
 
 export FLASK_PORT="5001"
 docker run --rm -d -e FLASK_PORT=$FLASK_PORT -p $FLASK_PORT:$FLASK_PORT kc_l2
+export FLASK_PORT="5002"
+docker run --rm -d -e FLASK_PORT=$FLASK_PORT -p $FLASK_PORT:$FLASK_PORT kc_l2
+export FLASK_PORT="5003"
+docker run --rm -d -e FLASK_PORT=$FLASK_PORT -p $FLASK_PORT:$FLASK_PORT kc_l2
