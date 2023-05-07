8afc3f2b7d83696e1b375b16c52743a2397f01cf
Valeriy <knyazvs85@gmail.com>
2023-05-07 18:38:30 +0300

    test commit

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import os
-import time
 import random
-import json
 from flask import Flask, request, jsonify
 from utils import get_secret_number
 import redis
+import signal
 import sys
+import json
+import time
+
 # from b2b_scoring import init_redis, init_scheduler, views
 
 URL_SEC_NUMBER = 'https://lab.karpov.courses/hardml-api/module-5/get_secret_number'
@@ -14,7 +16,16 @@ port = os.environ.get("FLASK_PORT")
 print('flask port', port)
 host_name = "95.216.191.176"
 app_name = "web_app"
-replic_name = "_".join(["replic1", host_name, port])
+replic_name = "_".join([app_name, host_name, port])
+
+def handler(sig, frame):
+    handler_redis_client = redis.Redis(host="172.17.0.2", port=6379, db=0, password='lolkek123')
+    handler_redis_client.lrem(app_name, 0, replic_name)
+    handler_redis_client.delete(replic_name)
+    sys.exit(0)
+
+signal.signal(signal.SIGTERM, handler)
+signal.signal(signal.SIGINT, handler)
 
 app = Flask(__name__)
 
--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 export FLASK_PORT="5001"
-docker run --rm -d -e FLASK_PORT=$FLASK_PORT -p $FLASK_PORT:$FLASK_PORT kc_l2
+docker run --rm -d -e FLASK_PORT=$FLASK_PORT -p --name web_app_1 $FLASK_PORT:$FLASK_PORT kc_l2
 export FLASK_PORT="5002"
-docker run --rm -d -e FLASK_PORT=$FLASK_PORT -p $FLASK_PORT:$FLASK_PORT kc_l2
+docker run --rm -d -e FLASK_PORT=$FLASK_PORT -p --name web_app_2 $FLASK_PORT:$FLASK_PORT kc_l2
 export FLASK_PORT="5003"
-docker run --rm -d -e FLASK_PORT=$FLASK_PORT -p $FLASK_PORT:$FLASK_PORT kc_l2
+docker run --rm -d -e FLASK_PORT=$FLASK_PORT -p --name web_app_3 $FLASK_PORT:$FLASK_PORT kc_l2
