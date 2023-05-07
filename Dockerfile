50f9cafc36d83d7817bcff2d10812da87d1f68a7
Valeriy <knyazvs85@gmail.com>
2023-05-07 12:38:08 +0300

    test commit

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,6 @@ COPY requirements.txt requirements.txt
 RUN pip install --upgrade pip && pip install -r requirements.txt
 COPY main.py main.py
 COPY utils.py utils.py
-EXPOSE 5000
+ENV FLASK_PORT=5000
+EXPOSE $FLASK_PORT
 CMD python ./main.py
--- main.py
+++ main.py
@@ -5,11 +5,12 @@ import json
 from flask import Flask, request, jsonify
 from utils import get_secret_number
 import redis
+import sys
 # from b2b_scoring import init_redis, init_scheduler, views
 
 URL_SEC_NUMBER = 'https://lab.karpov.courses/hardml-api/module-5/get_secret_number'
 
-
+port = os.environ.get("FLASK_PORT")
 app = Flask(__name__)
 
 @app.route('/return_secret_number')
@@ -23,16 +24,15 @@ if __name__ == '__main__':
     app_name = "web_app"
     replic_name = "replic1"
     host_name = "95.216.191.176"
-    port_name = 5000
 
-    # redis_client = redis.Redis(host="localhost", port=6379, db=0, password='lolkek123')
-    redis_client = redis.Redis(host="172.17.0.2", port=6379, db=0, password='lolkek123')
+    redis_client = redis.Redis(host="localhost", port=6379, db=0, password='lolkek123')
+    # redis_client = redis.Redis(host="172.17.0.2", port=6379, db=0, password='lolkek123')
     redis_client.rpush(app_name, replic_name)
 
 
     redis_client.hset(replic_name, "host", host_name)
-    redis_client.hset(replic_name, "port", port_name)
+    redis_client.hset(replic_name, "port", port)
 
     redis_client.close()
 
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=port)
