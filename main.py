042bfdbf622c064d751512ec049d23ec7f960818
Valeriy <knyazvs85@gmail.com>
2023-05-07 13:38:24 +0300

    test commit

--- main.py
+++ main.py
@@ -9,26 +9,25 @@ import sys
 # from b2b_scoring import init_redis, init_scheduler, views
 
 URL_SEC_NUMBER = 'https://lab.karpov.courses/hardml-api/module-5/get_secret_number'
-
 port = os.environ.get("FLASK_PORT")
+# port = "5004"
+print('flask port', port)
+host_name = "95.216.191.176"
+app_name = "web_app"
+replic_name = "_".join(["replic1", host_name, port])
+
 app = Flask(__name__)
 
 @app.route('/return_secret_number')
 def return_secret_number():
     return jsonify(secret_number=secret_number, random_number=random.randint(1, 10))
 
-
 if __name__ == '__main__':
     secret_number = get_secret_number(URL_SEC_NUMBER)
-
-    app_name = "web_app"
-    replic_name = "replic1"
-    host_name = "95.216.191.176"
 
     # redis_client = redis.Redis(host="localhost", port=6379, db=0, password='lolkek123')
     redis_client = redis.Redis(host="172.17.0.2", port=6379, db=0, password='lolkek123')
     redis_client.rpush(app_name, replic_name)
-
 
     redis_client.hset(replic_name, "host", host_name)
     redis_client.hset(replic_name, "port", port)
@@ -36,4 +35,3 @@ if __name__ == '__main__':
     redis_client.close()
 
     app.run(host="0.0.0.0", port=port)
-
